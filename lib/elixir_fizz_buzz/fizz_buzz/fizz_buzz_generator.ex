f789ea51930baf297ca7e8aaa4a79536e2ba5812
John Lewis <johnlewizz@gmail.com>
2023-11-07 19:18:15 +0000

    Add types to fizz_buzz_generator.ex

--- lib/elixir_fizz_buzz/fizz_buzz/fizz_buzz_generator.ex
+++ lib/elixir_fizz_buzz/fizz_buzz/fizz_buzz_generator.ex
@@ -5,7 +5,8 @@ defmodule FizzBuzz.FizzBuzzGenerator do
     {:ok, result}
   end
 
-  @spec get_between_values(start_value :: non_neg_integer(), end_value :: non_neg_integer()) :: {atom(), String.t()}
+  @spec get_between_values(start_value :: non_neg_integer(), end_value :: non_neg_integer()) ::
+          {atom(), String.t()}
   def get_between_values(start_value, end_value) when start_value > end_value do
     {:error, "Start value greater than end value"}
   end
@@ -15,23 +16,26 @@ defmodule FizzBuzz.FizzBuzzGenerator do
     {:ok, result}
   end
 
-  @spec fizz_buzz(start_value :: non_neg_integer(), end_value :: non_neg_integer()):: list()
+  @spec fizz_buzz(start_value :: non_neg_integer(), end_value :: non_neg_integer()) :: list()
   def fizz_buzz(start_value, end_value) do
     Enum.map(start_value..end_value, &fizz_buzz_check(&1))
   end
 
-  @spec fizz_buzz_check(value :: non_neg_integer()) :: String.t() | non_neg_integer()
-  defp fizz_buzz_check(value) when rem(value, 3) == 0 and rem(value, 5) == 0 do
-    "fizzbuzz"
+  @spec fizz_buzz_check(number :: non_neg_integer()) :: %{
+          id: non_neg_integer(),
+          value: String.t() | non_neg_integer()
+        }
+  defp fizz_buzz_check(number) when rem(number, 3) == 0 and rem(number, 5) == 0 do
+    %{id: number, value: "fizzbuzz"}
   end
 
-  defp fizz_buzz_check(value) when rem(value, 3) == 0 do
-    "fizz"
+  defp fizz_buzz_check(number) when rem(number, 3) == 0 do
+    %{id: number, value: "fizz"}
   end
 
-  defp fizz_buzz_check(value) when rem(value, 5) == 0 do
-    "buzz"
+  defp fizz_buzz_check(number) when rem(number, 5) == 0 do
+    %{id: number, value: "buzz"}
   end
 
-  defp fizz_buzz_check(value), do: value
+  defp fizz_buzz_check(number), do: %{id: number, value: number}
 end
